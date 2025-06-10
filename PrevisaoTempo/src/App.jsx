094123feff767f9c4b21f02d98f8c3e92bd81129
henrique-castilho <henrique.castilho.pires@gmail.com>
2025-06-10 16:08:44 -0300

    feat(p2): implementa componente de busca com debounce

--- PrevisaoTempo/src/App.jsx
+++ PrevisaoTempo/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Busca from './components/Busca'
 
 function App() {
 
   return (
     <div>
-      
+      <Busca/>
     </div>
   )
 }
---
+++ PrevisaoTempo/src/components/Busca.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function Busca() {
+    const [termoDeBusca, setTermoDeBusca] = useState('São Paulo')
+
+    useEffect(() => {
+        if (termoDeBusca.length < 3) {
+            return
+        }
+
+        const fazerBusca = async () => {
+            try {
+                const response = await axios.get('http://localhost:3001/buscar', {
+                    params: { query: termoDeBusca }
+                })
+                console.log(response.data)
+
+            } catch (error) {
+                console.error('Erro na busca:', error)
+            }
+        }
+
+        const timeoutID = setTimeout(() => {
+            fazerBusca()
+        }, 2000)
+        return () => {
+            clearTimeout(timeoutID)
+        }
+    }, [termoDeBusca])
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={termoDeBusca}
+                onChange={(e) => setTermoDeBusca(e.target.value)}>
+            </input>
+        </div>
+    )   
+}
+
+export default Busca;
