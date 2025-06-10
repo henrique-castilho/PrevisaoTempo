274a221eab02cb5c2b07c1735aad52c866791cc8
henrique-castilho <henrique.castilho.pires@gmail.com>
2025-06-10 17:24:05 -0300

    fix(p2): ajuste no componente busca

--- PrevisaoTempo/src/components/Busca.jsx
+++ PrevisaoTempo/src/components/Busca.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-function Busca() {
+const Busca = () => {
     const [termoDeBusca, setTermoDeBusca] = useState('São Paulo')
 
     useEffect(() => {
